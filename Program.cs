2fa1f7024ff775131146e316d502657dd5f9c277
sathyagimenes <sathya.gimenes@gmail.com>
2023-05-24 21:18:24 -0300

    outra alteracao realizada por sathya

--- Program.cs
+++ Program.cs
@@ -1,27 +1,28 @@
 ﻿// Alteração realizada por sathya
+// Outra alteração realizada por sathya
 using System;
 
 class Program
 {
-    static void Main()
-    {
-        int number = 0;
-        for (int i = 1; i <= 1000; i++)
-        {
-            if (i % 7 == 0 && i % 13 == 0 && i % 17 == 0)
-            {
-                number = i;
-                break;
-            }
-        }
+	static void Main()
+	{
+		int number = 0;
+		for (int i = 1; i <= 1000; i++)
+		{
+			if (i % 7 == 0 && i % 13 == 0 && i % 17 == 0)
+			{
+				number = i;
+				break;
+			}
+		}
 
-        if (number != 0)
-        {
-            Console.WriteLine("O primeiro número divisível por 7, 13 e 17 é: " + number);
-        }
-        else
-        {
-            Console.WriteLine("Nenhum número divisível por 7, 13 e 17 encontrado entre 1 e 1000.");
-        }
-    }
+		if (number != 0)
+		{
+			Console.WriteLine("O primeiro número divisível por 7, 13 e 17 é: " + number);
+		}
+		else
+		{
+			Console.WriteLine("Nenhum número divisível por 7, 13 e 17 encontrado entre 1 e 1000.");
+		}
+	}
 }
